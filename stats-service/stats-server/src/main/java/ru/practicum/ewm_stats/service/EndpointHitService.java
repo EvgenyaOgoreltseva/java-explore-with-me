e71e02ef633e26422b180b2b7b60b926c6b7b937
Evgenya Ogoreltseva <zhenyakirov43555@gmail.com>
2023-09-18 14:02:03 +0300

    Закончила работу над этапом 1

--- stats-service/stats-server/src/main/java/ru/practicum/ewm_stats/service/EndpointHitService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm_stats/service/EndpointHitService.java
@@ -7,7 +7,6 @@ import ru.practicum.ewm_stats.dto.ViewStatsDto;
 import java.util.List;
 
 public interface EndpointHitService {
-
     EndpointHitDto saveHit(EndpointHitDto endpointHitDto);
 
     List<ViewStatsDto> getViewStats(GetStatsDto getStatsDto);
