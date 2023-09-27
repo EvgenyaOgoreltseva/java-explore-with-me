48835448fb714188619a05f4475396682dc3641a
Evgenya Ogoreltseva <zhenyakirov43555@gmail.com>
2023-09-27 03:08:30 +0300

    Закончила работу над этапом 2

---
+++ main-service/src/main/java/ru/practicum/category/controller/CategoryPublicController.java
@@ -0,0 +1,32 @@
+package ru.practicum.category.controller;
+
+import com.sun.nio.sctp.IllegalReceiveException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.category.model.Category;
+import ru.practicum.category.service.CategoryService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/categories")
+public class CategoryPublicController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> findAllCategory(@RequestParam(value = "from", defaultValue = "0", required = false) Integer from,
+                                          @RequestParam(value = "size", defaultValue = "10", required = false) Integer size) {
+
+        if (size <= 0 || from < 0) {
+            throw new IllegalReceiveException("Неверно указан параметр");
+        }
+        return categoryService.findAllCategory(from, size);
+    }
+
+    @GetMapping("/{catId}")
+    public Category getCategoryById(@PathVariable Long catId) {
+        return categoryService.getById(catId);
+    }
+}
---
+++ main-service/src/main/java/ru/practicum/compilation/controller/CompilationsPublicController.java
@@ -0,0 +1,35 @@
+package ru.practicum.compilation.controller;
+
+import com.sun.nio.sctp.IllegalReceiveException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.compilation.dto.CompilationDto;
+import ru.practicum.compilation.service.CompilationService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/compilations")
+public class CompilationsPublicController {
+    private final CompilationService compilationsService;
+
+    @GetMapping("/{compId}")
+    public CompilationDto getCompilationById(@PathVariable Long compId) {
+        return compilationsService.getCompilationById(compId);
+    }
+
+    @GetMapping
+    public List<CompilationDto> getCompilationsByPinned(@RequestParam(defaultValue = "false") Boolean pinned,
+                                                   @RequestParam(value = "from", defaultValue = "0", required = false) Integer from,
+                                                   @RequestParam(value = "size", defaultValue = "10", required = false) Integer size) {
+
+        if (size <= 0 || from < 0) {
+            throw new IllegalReceiveException("Неверно указан параметр");
+        }
+
+        return compilationsService.getCompilationsByPinned(pinned, from, size);
+    }
+
+
+}
