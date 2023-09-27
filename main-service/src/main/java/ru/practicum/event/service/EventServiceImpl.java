438a72b1589ee916b31beba415c64918e53d057d
Evgenya Ogoreltseva <zhenyakirov43555@gmail.com>
2023-09-27 10:41:40 +0300

    Закончила работу над этапом 2

--- main-service/src/main/java/ru/practicum/event/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/event/service/EventServiceImpl.java
@@ -39,6 +39,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
+    
     private final ViewStatsClient viewStatsClient;
     private static final String APP = "ewm-main-service";
     private static final String LOWER_DATE = "1970-01-01 00:00:00";
